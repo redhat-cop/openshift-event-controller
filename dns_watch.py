0cd21aec0b637b16543f9c4569a705a67dfea678
Øystein Bedin <bedin@redhat.com>
2016-07-22 12:41:50 -0600

    Updated DNS watcher

--- dns_watch.py
+++ dns_watch.py
@@ -3,15 +3,8 @@
 import os
 import requests
 import json
-import pkiutils
 import subprocess
-from OpenSSL import crypto
 from OpenShiftWatcher import OpenShiftWatcher
-
-router_ipv4 = os.environ['OS_ROUTER_IPV4']
-
-dns_server = os.environ['DNS_SERVER']
-dns_keyfile = os.environ['DNS_KEY_FILE']
 
 
 def watch_routes():
@@ -22,24 +15,50 @@ def watch_routes():
 
     for event in watcher.stream():
         if type(event) is dict and 'type' in event:
-            try:
-                dns_a_record = event['object']['spec']['host']
+            if event['type'] == 'ADDED':
+                add_dns(event)
+            elif event['type'] == 'MODIFIED':
+                route = get_route(event)
+                if route.status_code == 404:
+                    remove_dns(event)
+                else:
+                    print 'Route \'{0}\' modified'.format(event['object']['metadata']['name'])
 
-                command  = "server %s\n" % dns_server
-                command += "update delete %s A\n" % dns_a_record
 
-                if event['type'] == 'ADDED':
-                    command += "update add %s 180    A %s\n" % (dns_a_record, router_ipv4)
+def get_route(event):
+    route_name = event['object']['metadata']['name']
 
-                command += "send\n"
-                command = "nsupdate -k {0} -v << EOF\n{1}\nEOF\n".format(dns_keyfile, command)
+    req = requests.get('https://{0}/oapi/v1/namespaces/{1}/routes/{2}'.format(os.environ['OS_API'], os.environ['OS_NAMESPACE'], route_name),
+                       headers={'Authorization': 'Bearer {0}'.format(os.environ['OS_TOKEN']), 'Content-Type':'application/strategic-merge-patch+json'},
+                       verify=False
+                       )
 
-                print("Calling the following command now:\n\n" + command)
-  
-                subprocess.call(command, shell=True)
+    return req
+ 
 
-            except Exception as e:
-                print "Exception: ", e
-                continue
+def modify_dns(action):
+    try:
+        command  = "server %s\n" % os.environ['DNS_SERVER']
+        command += action
+        command += "send\n"
+        command = "nsupdate -k {0} -v << EOF\n{1}\nEOF\n".format(os.environ['DNS_KEY_FILE'], command)
+
+        subprocess.call(command, shell=True)
+
+    except Exception as e:
+        print "Exception: ", e
+
+
+def remove_dns(event):
+    dns_a_record = event['object']['spec']['host']
+    action = "update delete %s A\n" % dns_a_record
+    modify_dns(action)
+
+
+def add_dns(event):
+    dns_a_record = event['object']['spec']['host']
+    action = "update add %s 180  A %s\n" % (dns_a_record, os.environ['OS_ROUTER_IPV4'])
+    modify_dns(action)
+
 
 watch_routes()
