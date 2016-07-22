f6ade46eefe8df8cc1d9d937170350d1f4b6296f
Eric Sauer <esauer@redhat.com>
2016-07-22 11:59:41 -0400

    blah

--- route_watch.py
+++ route_watch.py
@@ -22,6 +22,7 @@ def watch_routes():
                                os_auth_token=os.environ['OS_TOKEN'])
 
     for event in watcher.stream():
+        print event
         if type(event) is dict and 'type' in event and event['type'] == 'ADDED':
             print event
             print
@@ -36,44 +37,53 @@ def watch_routes():
             except Exception as e:
                 print "Create CSR Exception: ", e
 
-            #TODO: Sign Request with Dynamic CA (IPA)
-            # authenticate to IPA Server
-            try:
-                resp = session.post('{0}session/login_password'.format(ipaurl),
-                                params="", data = {'user':ipa_user,'password':ipa_password}, verify=False,
-                                headers={'Content-Type':'application/x-www-form-urlencoded', 'Accept':'applicaton/json'})
-            except Exception as e:
-                print "IPA Auth Exception: ", e
+class ipa_api(object):
 
+    def __init__(self,ipa_user,ipa_password,ipa_host):
+        self.session = requests.Session()
+        self.ipa_url = ipa_host
+        #TODO: Sign Request with Dynamic CA (IPA)
+        # authenticate to IPA Server
+        try:
+            resp = session.post('{0}session/login_password'.format(ipaurl),
+                            params="", data = {'user':ipa_user,'password':ipa_password}, verify=False,
+                            headers={'Content-Type':'application/x-www-form-urlencoded', 'Accept':'applicaton/json'})
+        except Exception as e:
+            print "IPA Auth Exception: ", e
 
-            try:
-                header={'referer': ipaurl, 'Content-Type':'application/json', 'Accept':'application/json'}
+        self.header = {'referer': ipaurl, 'Content-Type':'application/json', 'Accept':'application/json'}
 
-                # CREATE HOST
-                create_host = session.post('{0}session/json'.format(ipaurl), headers=header,
-                                           data=json.dumps({'id': 0, 'method': 'host_add', 'params': [[event['object']['spec']['host']], {'force': True}]}), verify=False)
+    def create_host(self,host):
+                try:
 
-                print "    Host Create Return Code: {0}".format(create_host.status_code)
-            except Exception as e:
-                print "Create Host Exception: ", e
+                    # CREATE HOST [event['object']['spec']['host']]
+                    create_host = self.session.post('{0}session/json'.format(self.ipa_url), headers=self.header,
+                                               data=json.dumps({'id': 0, 'method': 'host_add', 'params': [host, {'force': True}]}), verify=False)
 
-            try:
-                # CREATE CERT
-                cert_request = session.post('{0}session/json'.format(ipaurl), headers=header,
-                                            data=json.dumps({'id': 0, 'method': 'cert_request', 'params': [[csr], {'principal': 'host/{0}@{1}'.format(event['object']['spec']['host'], realm),
-                                                                                                                   'request_type': 'pkcs10', 'add': False}]}), verify=False)
+                    print "    Host Create Return Code: {0}".format(create_host.status_code)
+                except Exception as e:
+                    print "Create Host Exception: ", e
 
-                print "    Certificate Signing Return Code: {0}".format(cert_request.status_code)
-                #print "  {0}".format(cert_request.json())
-                cert_resp = cert_request.json()
-            except Exception as e:
-                print "Cert Create Exception", e
+    def create_cert(self):
 
-            print "CERTIFICATE:\n-----BEGIN CERTIFICATE-----\n{0}\n-----END CERTIFICATE-----".format(
-                '\n'.join(cert_resp['result']['result']['certificate'][i:i+65] for i in xrange(0, len(cert_resp['result']['result']['certificate']), 65)))
-            print
-            print "KEY:\n {0}".format(key.exportKey('PEM'))
+                try:
+                    # CREATE CERT
+                    cert_request = self.session.post('{0}session/json'.format(self.ipa_url), headers=header,
+                                                data=json.dumps({'id': 0, 'method': 'cert_request', 'params': [[csr], {'principal': 'host/{0}@{1}'.format(event['object']['spec']['host'], realm),
+                                                                                                                       'request_type': 'pkcs10', 'add': False}]}), verify=False)
 
+                    print "    Certificate Signing Return Code: {0}".format(cert_request.status_code)
+                    #print "  {0}".format(cert_request.json())
+                    cert_resp = cert_request.json()
+                except Exception as e:
+                    print "Cert Create Exception", e
+
+                print "CERTIFICATE:\n-----BEGIN CERTIFICATE-----\n{0}\n-----END CERTIFICATE-----".format(
+                    '\n'.join(cert_resp['result']['result']['certificate'][i:i+65] for i in xrange(0, len(cert_resp['result']['result']['certificate']), 65)))
+                print
+                print "KEY:\n {0}".format(key.exportKey('PEM'))
+
+def update_route():
             try:
                 #TODO: Update Route
                 req = requests.patch('https://{0}/oapi/v1/namespaces/{1}/routes/{2}'.format(os.environ['OS_API'], os.environ['OS_NAMESPACE'], event['object']['metadata']['name']),
@@ -86,5 +96,6 @@ def watch_routes():
                 print "    OpenShift Route Update Return Code: {0}".format(req.status_code)
             except Exception as e:
                 print "Route update exception: ", e
+
 
 watch_routes()
