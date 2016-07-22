16d4c9ebb5d5ac558b6c1e62aaca3ab95b79edff
Eric Sauer <esauer@redhat.com>
2016-07-22 14:48:06 -0400

    ca_trust

--- OpenShiftWatcher/__init__.py
+++ OpenShiftWatcher/__init__.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 class OpenShiftWatcher(object):
-    def __init__(self, os_api_endpoint, os_resource, os_namespace, os_auth_token):
+    def __init__(self, os_api_endpoint, os_resource, os_namespace, os_auth_token, ca_trust='/etc/ssl/certs/ca-bundle.trust.crt'):
         ''' os_auth_token generated from `oc whoami -t`
 
             Example:
@@ -14,17 +14,19 @@ class OpenShiftWatcher(object):
         self.os_api_url = "https://{0}/oapi/v1/namespaces/{1}/{2}?watch=true".format(os_api_endpoint, os_namespace, os_resource)
         self.os_auth_token = os_auth_token
         self.session = requests.Session()
+        self.ca_trust = ca_trust
 
     def stream(self):
         req = requests.Request("GET", self.os_api_url,
                                headers={'Authorization': 'Bearer {0}'.format(self.os_auth_token)},
-                               params="").prepare()
+                               params=""
+                               ).prepare()
 
-        resp = self.session.send(req, stream=True, verify=False)
+        resp = self.session.send(req, stream=True, verify=self.ca_trust)
 
         if resp.status_code != 200:
             raise Exception("Unable to contact OpenShift API. Message from server: {0}".format(resp.text))
-            
+
         for line in resp.iter_lines():
             if line:
                 try:
