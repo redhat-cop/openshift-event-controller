81f37f4e201568488957ed17fe612f0f7de81b8d
Eric Sauer <esauer@redhat.com>
2017-03-02 21:59:48 -0500

    Adding PKI plugin specific to IPA/IDM server

--- IPAClient/__init__.py
+++ IPAClient/__init__.py
@@ -5,7 +5,7 @@ from OpenSSL import crypto
 
 
 class IPAClient(object):
-    def __init__(self, ipa_user, ipa_password, ipa_url, bits=2048, ca_trust='/etc/ssl/certs/ca-bundle.trust.crt'):
+    def __init__(self, ipa_user, ipa_password, ipa_url, bits=2048, ca_trust=False):
         #ipaurl="https://idm-1.etl.lab.eng.rdu2.redhat.com/ipa/"
         #realm="ETL.LAB.ENG.RDU2.REDHAT.COM"
 
@@ -32,8 +32,15 @@ class IPAClient(object):
     def create_host(self, host):
         try:
             # CREATE HOST [event['object']['spec']['host']]
+            request_payload = {'id': 0, 'method': 'host_add', 'params': [[host],{}]}
             create_host = self.session.post('{0}session/json'.format(self.ipa_url), headers=self.header,
-                                            data=json.dumps({'id': 0, 'method': 'host_add', 'params': [host, {'force': True}]}), verify=self.ca_trust)
+                                            data=json.dumps(request_payload), verify=self.ca_trust)
+            if create_host.json()['error']:
+                if create_host.json()['error']['name'] == 'DuplicateEntry':
+                    # Host already created, we can continue
+                    pass
+                else:
+                    raise Exception("Create Host Failed: {0}\n{1}".format(create_host.json(), json.dumps(request_payload, indent=2)))
         except Exception as e:
             raise Exception("Create Host Exception: {0}".format(e))
 
@@ -71,5 +78,12 @@ class IPAClient(object):
                                              verify=self.ca_trust)
             cert_resp = cert_request.json()
         except Exception as e:
-            raise Exception("Cert Create Exception: {0}".format(e))
-        return cert_resp['result']['result']['certificate'], key
+            raise Exception("Cert Create Exception: {0}\n{1}".format(e, cert_request))
+
+        try:
+            return cert_resp['result']['result']['certificate'], key
+        except TypeError as e:
+            if cert_resp['error']:
+                raise Exception("Key Error: {0}\nCert Request Body:{1}\nCert Response: {2}\nKey: {3}".format(e, cert_request, cert_resp, key))
+            else:
+                raise Exception("Unknown Exception {0}".format(e))
---
+++ plugin_ipa/README.md
@@ -0,0 +1,53 @@
+# Event Watcher PKI Plugin
+
+This plugin provides functionality to automatically secure routes with certificates generated from an external PKI system.
+
+## Quickstart
+
+Write the following to your config.ini:
+```
+...(see watcher readme for global settings)...
+
+[plugin_ipa]
+need_cert_annotation=openshift.io/managed.cert
+ipa_user=<ipa/idm username>
+ipa_password=<ipa/idm password>
+ipa_url=<ipa/idm API URL ex: https://idm.example.com/ipa/>
+ipa_realm=<realm name>
+ca_trust=</path/to/ca.crt>
+```
+
+Then run the following command:
+
+```
+python3 watch.py --config conf/config.ini
+```
+
+## Plugin Configuration Options
+
+| ini Variable | Required | Description |
+| ------------- | ------------- | -------------|
+| need_cert_annotation | false | Name of the annotation used to designate routes that need certificates |
+| ipa_user | true | IPA/IDM user with permissions to create hosts & certificates |
+| ipa_password | true | IPA/IDM password |
+| ipa_url | true | Url to IPA/IDM API (include path) |
+| ipa_realm | true | Realm under which new hosts/certs should be created |
+| ca_trust | false | CA certificate for IPA server trust |
+## Developer local Setup
+
+### Dependencies
+
+First, install the following packages:
+
+```
+sudo dnf install "C Development Tools and Libraries"
+sudo dnf install openssl-devel
+sudo dnf install python3-pip
+sudo dnf install python3-devel
+```
+
+Then, install the following python modules
+```
+sudo pip3 install cryptography
+sudo pip3 install pkiutils
+```
